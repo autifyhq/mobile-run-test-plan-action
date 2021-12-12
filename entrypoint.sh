6cb5ac9e0d45ae23c48dff510dbefb4d14e14273
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2021-12-12 23:52:23 +0900

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -l
 
 readonly API_ENDPOINT="https://mobile-app.autify.com/api/v1"
-readonly AUTIFY_PROJECT_ID=${1}
-readonly AUTIFY_TEST_PLAN_ID=${2}
-readonly AUTIFY_BUILD_ID=${3}
+readonly AUTIFY_TEST_PLAN_ID=${1}
+readonly AUTIFY_BUILD_ID=${2}
 
 info() {
   echo -e "$1"
@@ -28,14 +27,10 @@ main() {
     EOF
     ) \
     "https://mobile-app.autify.com/api/v1/test_plans/${AUTIFY_TEST_PLAN_ID}/test_plan_results")
-
-  test_plan_result_id=$(echo "${response}" | jq -r .id)
-  echo -e "\033[00;32m * https://mobile-app.autify.com/projects/${AUTIFY_PROJECT_ID}/results/${test_plan_result_id} \033[0m"
 }
 
 # parameters
 info "parameters:"
-info "* project_id: ${AUTIFY_PROJECT_ID}"
 info "* test_plan_id: ${AUTIFY_TEST_PLAN_ID}"
 info "* build_id: ${AUTIFY_BUILD_ID}"
 
