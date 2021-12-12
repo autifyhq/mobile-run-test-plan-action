bc0346fc6af6d662fa569f4b0eca6765bc893ae9
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2021-12-13 02:08:15 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,14 @@ readonly API_ENDPOINT="https://mobile-app.autify.com/api/v1"
 readonly BUILD_ID=${1}
 readonly TEST_PLAN_ID=${2}
 
+success() {
+  echo -e "\033[00;32m $1 \033[0m"
+}
+
+error() {
+  echo -e "\033[00;31m $1 \033[0m"
+}
+
 run_test_plan() {
   echo $(curl -s -X POST \
     -H "Authorization: Bearer ${AUTIFY_FOR_MOBILE_API_TOKEN}" \
@@ -16,12 +24,14 @@ main() {
   response=$(run_test_plan)
   
   if [[ "$( echo $response | jq 'has("errors")' )" == "true" ]]; then
+    error "Failed to run test plan"
     echo $response | jq .
     exit 1
   fi
 
+  success "Test plan ran"
   echo $response | jq .
-} 
+}
 
 # run
 main "$@"
